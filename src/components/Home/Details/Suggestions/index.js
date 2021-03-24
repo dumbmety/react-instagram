baf384fe0c044b74ed737171700c3ddc0fe882bb
Mehdi Neysi <dev.mehdineysi@gmail.com>
2021-03-24 19:09:15 +0430

    feat: added classes for suggestions

--- src/components/Home/Details/Suggestions/index.js
+++ src/components/Home/Details/Suggestions/index.js
@@ -1,117 +1,85 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './index.css';
+
 const Suggestions = () => {
   return (
-    <section className="mb-5">
-      <header className="mb-3 flex items-center justify-between">
-        <h2 className="text-sm font-semibold text-gray-500">
-          Suggestions For You
-        </h2>
-        <Link to="/" className="text-sm text-gray-800">
-          See All
-        </Link>
+    <section className="suggestions">
+      <header>
+        <h2>Suggestions For You</h2>
+        <Link to="/">See All</Link>
       </header>
-      <ul className="space-y-3">
-        <li className="flex items-center justify-between">
-          <div className="flex items-center">
+      <ul>
+        <li className="user">
+          <div className="user__content">
             <Link to="/">
-              <img
-                className="w-10 h-10 mr-3 rounded-full object-cover"
-                src="/img/users/ryanswich.jpg"
-                alt="ryanswich"
-              />
+              <img src="/img/users/ryanswich.jpg" alt="ryanswich" />
             </Link>
-            <div className="flex flex-col">
-              <Link to="/" className="text-sm font-semibold hover:underline">
+            <div className="text">
+              <Link to="/" className="username">
                 ryanswich
               </Link>
-              <span className="text-xs text-gray-500">New to Instagram</span>
+              <span className="status">New to Instagram</span>
             </div>
           </div>
-          <button className="text-sm text-lightBlue-500 font-semibold">
-            Follow
-          </button>
+          <button>Follow</button>
         </li>
-        <li className="flex items-center justify-between">
-          <div className="flex items-center">
+        <li className="user">
+          <div className="user__content">
             <Link to="/">
-              <img
-                className="w-10 h-10 mr-3 rounded-full object-cover"
-                src="/img/users/jakobowsky.jpg"
-                alt="jakobowsky"
-              />
+              <img src="/img/users/jakobowsky.jpg" alt="jakobowsky" />
             </Link>
-            <div className="flex flex-col">
-              <Link to="/" className="text-sm font-semibold hover:underline">
+            <div className="text">
+              <Link to="/" className="username">
                 jakobowsky
               </Link>
-              <span className="text-xs text-gray-500">New to Instagram</span>
+              <span className="status">New to Instagram</span>
             </div>
           </div>
-          <button className="text-sm text-lightBlue-500 font-semibold">
-            Follow
-          </button>
+          <button>Follow</button>
         </li>
-        <li className="flex items-center justify-between">
-          <div className="flex items-center">
+        <li className="user">
+          <div className="user__content">
             <Link to="/">
-              <img
-                className="w-10 h-10 mr-3 rounded-full object-cover"
-                src="/img/users/joeel56.jpg"
-                alt="joeel56"
-              />
+              <img src="/img/users/joeel56.jpg" alt="joeel56" />
             </Link>
-            <div className="flex flex-col">
-              <Link to="/" className="text-sm font-semibold hover:underline">
+            <div className="text">
+              <Link to="/" className="username">
                 joeel56
               </Link>
-              <span className="text-xs text-gray-500">New to Instagram</span>
+              <span className="status">New to Instagram</span>
             </div>
           </div>
-          <button className="text-sm text-lightBlue-500 font-semibold">
-            Follow
-          </button>
+          <button>Follow</button>
         </li>
-        <li className="flex items-center justify-between">
-          <div className="flex items-center">
+        <li className="user">
+          <div className="user__content">
             <Link to="/">
-              <img
-                className="w-10 h-10 mr-3 rounded-full object-cover"
-                src="/img/users/andrew.eugene.jpg"
-                alt="andrew.eugene"
-              />
+              <img src="/img/users/andrew.eugene.jpg" alt="andrew.eugene" />
             </Link>
-            <div className="flex flex-col">
-              <Link to="/" className="text-sm font-semibold hover:underline">
+            <div className="text">
+              <Link to="/" className="username">
                 andrew.eugene
               </Link>
-              <span className="text-xs text-gray-500">New to Instagram</span>
+              <span className="status">New to Instagram</span>
             </div>
           </div>
-          <button className="text-sm text-lightBlue-500 font-semibold">
-            Follow
-          </button>
+          <button>Follow</button>
         </li>
-        <li className="flex items-center justify-between">
-          <div className="flex items-center">
+        <li className="user">
+          <div className="user__content">
             <Link to="/">
-              <img
-                className="w-10 h-10 mr-3 rounded-full object-cover"
-                src="/img/users/marziuxd.jpg"
-                alt="marziuxd"
-              />
+              <img src="/img/users/marziuxd.jpg" alt="marziuxd" />
             </Link>
-            <div className="flex flex-col">
-              <Link to="/" className="text-sm font-semibold hover:underline">
+            <div className="text">
+              <Link to="/" className="username">
                 marziuxd
               </Link>
-              <span className="text-xs text-gray-500">New to Instagram</span>
+              <span className="status">New to Instagram</span>
             </div>
           </div>
-          <button className="text-sm text-lightBlue-500 font-semibold">
-            Follow
-          </button>
+          <button>Follow</button>
         </li>
       </ul>
     </section>
