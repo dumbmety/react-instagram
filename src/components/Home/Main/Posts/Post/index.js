76586fa028143a70ada3c02bb32b69a69b734794
Mehdi Neysi <dev.mehdineysi@gmail.com>
2021-03-24 21:29:09 +0430

    feat: added post classes

--- src/components/Home/Main/Posts/Post/index.js
+++ src/components/Home/Main/Posts/Post/index.js
@@ -9,75 +9,65 @@ import {
   IoShareSocialOutline
 } from 'react-icons/io5';
 
+import './index.css';
+
 const Post = ({ name, username, imgName, likes, time }) => {
   return (
-    <article className="bg-white rounded border border-gray-200 mb-5">
-      <header className="py-2 px-5 border-b border-gray-200 flex items-center justify-between">
-        <Link to="/" className="flex items-center">
-          <div className="bg-gradient-to-tr from-yellow-400 to-fuchsia-600 p-0.5 mr-2 rounded-full">
-            <div className="bg-white p-0.5 rounded-full">
-              <img
-                className="w-8 h-8 object-cover rounded-full"
-                src={`/img/users/${username}.jpg`}
-                alt={name}
-              />
+    <article className="post">
+      <header className="post__header">
+        <Link to="/" className="post__account">
+          <div className="image">
+            <div className="image__wrapper">
+              <img src={`/img/users/${username}.jpg`} alt={name} />
             </div>
           </div>
-          <span className="font-semibold text-sm hover:underline">
-            {username}
-          </span>
+          <span className="post__username">{username}</span>
         </Link>
-        <button className="focus:outline-none">
+        <button className="post__menu">
           <IoEllipsisHorizontalSharp size="1.2rem" />
         </button>
       </header>
       <div>
         <img
-          className="w-full h-full object-cover"
+          className="post__image"
           src={`/img/posts/${username}/${imgName}.jpg`}
           alt="Dashboard"
         />
       </div>
-      <footer className="py-2 px-5 space-y-2">
-        <div className="flex items-center justify-between">
-          <div className="flex items-center space-x-3">
-            <button className="flex items-center focus:outline-none">
+      <footer className="post__footer">
+        <div className="post__actions">
+          <div className="post__actions--left">
+            <button className="post__action">
               <IoHeartOutline size="1.5rem" />
             </button>
-            <button className="flex items-center focus:outline-none">
+            <button className="post__action">
               <IoChatbubbleOutline size="1.5rem" />
             </button>
-            <button className="flex items-center focus:outline-none">
+            <button className="post__action">
               <IoShareSocialOutline size="1.5rem" />
             </button>
           </div>
-          <div>
-            <button className="flex items-center focus:outline-none">
-              <IoBookmarkOutline size="1.5rem" />
-            </button>
-          </div>
+          <button className="post__action">
+            <IoBookmarkOutline size="1.5rem" />
+          </button>
         </div>
-        <Link to="/" className="block font-semibold text-sm">
+        <Link to="/" className="post__likes">
           {likes} likes
         </Link>
-        <Link
-          to="/"
-          className="text-gray-400 uppercase"
-          style={{ fontSize: '10px' }}
-        >
+        <Link to="/" className="post__time">
           {time}
         </Link>
       </footer>
-      <form className="flex items-center justify-between p-5 border-t border-gray-200">
-        <button type="button" className="focus:outline-none">
+      <form className="addComment">
+        <button type="button" className="addComment__emoji">
           <IoHappyOutline size="1.5rem" />
         </button>
         <input
-          className="flex-1 ml-3 leading-relaxed focus:outline-none"
           type="text"
+          className="addComment__message"
           placeholder="Add a comment..."
         />
-        <button type="submit" className="text-lightBlue-500 font-semibold">
+        <button type="submit" className="addComment__send">
           Post
         </button>
       </form>
