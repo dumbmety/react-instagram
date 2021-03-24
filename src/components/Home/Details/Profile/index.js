395308a1d4417b574de05bc42f5806805318368a
Mehdi Neysi <dev.mehdineysi@gmail.com>
2021-03-24 18:34:03 +0430

    feat: added profile details classes

--- src/components/Home/Details/Profile/index.js
+++ src/components/Home/Details/Profile/index.js
@@ -1,29 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './index.css';
+
 const Profile = () => {
   return (
-    <header className="my-5 flex items-center justify-between">
+    <header className="profile">
       <div className="flex items-center">
-        <Link to="/" className="bg-gray-300 p-0.5 rounded-full mr-5">
-          <div className="bg-white p-0.5 rounded-full w-14 h-14 overflow-hidden">
-            <img
-              className="w-full h-full object-cover rounded-full"
-              src="/img/users/neysidev.jpeg"
-              alt="Mehdi Neysi"
-            />
+        <Link to="/" className="profile__image">
+          <div className="profile__image--wrapper">
+            <img src="/img/users/neysidev.jpeg" alt="Mehdi Neysi" />
           </div>
         </Link>
         <div>
-          <Link to="/profile" className="font-semibold text-sm">
+          <Link to="/profile" className="profile__username">
             neysidev
           </Link>
-          <h2 className="font-semibold text-sm text-gray-400">Mehdi Neysi</h2>
+          <h3 className="profile__name">Mehdi Neysi</h3>
         </div>
       </div>
-      <button className="font-semibold text-sm text-lightBlue-500 focus:outline-none">
-        Switch
-      </button>
+      <button className="profile__button">Switch</button>
     </header>
   );
 };
