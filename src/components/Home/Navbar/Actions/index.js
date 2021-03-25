54660074074f6931c03e801dcc62bf65242f53c6
Mehdi Neysi <dev.mehdineysi@gmail.com>
2021-03-25 12:04:12 +0430

    refactor: navbar component classes

--- src/components/Home/Navbar/Actions/index.js
+++ src/components/Home/Navbar/Actions/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './index.css';
+
 const Actions = () => {
   return (
     <div id="main-header__mobile--actions">
--- src/components/Home/Navbar/Links/index.js
+++ src/components/Home/Navbar/Links/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './index.css';
+
 const Links = () => {
   return (
     <div>
