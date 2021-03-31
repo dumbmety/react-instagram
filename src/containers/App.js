64f0436d852d7715e8f253f16eabc91358b265a8
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2021-03-31 23:42:41 +0430

    Update: moved Navbar to App.js & Rendered it anywhere in application

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,12 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from '../components/Home/Navbar/Navbar';
 import Home from '../components/Home/Home';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-      </Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/" component={Navbar} />
     </Router>
   );
 };
