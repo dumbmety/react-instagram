e15c3b58d7c3757d0151808f85cc6b8eee14f618
Mehdi Neysi <dev.mehdineysi@gmail.com>
2022-04-08 21:48:22 +0430

    feat: implement stories with scroll

---
+++ src/constants/stories.ts
@@ -0,0 +1,11 @@
+export const USER_STORIES = [
+  { _id: "6250414ab8df8591dd07a5a5", username: "russcodes" },
+  { _id: "625042d3b224a02f3ba6af2e", username: "onlysiamak" },
+  { _id: "6250435b7d72373e7bc543e2", username: "sepide_moqadasi" },
+  { _id: "62504355f2319d44346d9cbe", username: "ui.amjad" },
+  { _id: "625043508410226f2682b39b", username: "mehradhiddenofficial" },
+  { _id: "6250434b7fb183398e50fd2a", username: "fazurrehman" },
+  { _id: "625043450b1e79bb83a6fad1", username: "sajon.co" },
+  { _id: "62504745f91e4741b1dbb4f7", username: "samirarahimi" },
+  { _id: "62504792f022aa1979045da5", username: "sinamehraad" }
+]
--- src/pages/Home/Main/Story.jsx
+++ src/pages/Home/Main/Story.jsx
@@ -1,110 +1,71 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi"
 
-const Story = () => {
+import { USER_STORIES } from "../../../constants/stories"
+
+// TODO: auto scroll to the start/end
+export default function Story() {
+  const usersContainerWidth = USER_STORIES.length * 64
+  const [translateX, setTranslateX] = useState(0)
+
+  const gotoNext = () => {
+    setTranslateX(translateX - 264)
+  }
+
+  const gotoPrev = () => {
+    setTranslateX(translateX + 264)
+  }
+
   return (
-    <section className="bg-white w-full border border-gray-200 rounded px-5 py-3 mb-5">
-      <ul className="flex space-x-4">
-        <li className="w-16 overflow-hidden space-y-5 text-center">
-          <Link to="/">
-            <div className="bg-gradient-to-tr from-yellow-400 to-fuchsia-600 p-0.5 rounded-full">
-              <div className="bg-white p-0.5 rounded-full">
-                <img
-                  className="w-full h-full object-cover rounded-full"
-                  src="/img/users/russcodes.jpg"
-                  alt="russcodes"
-                />
+    <section className="bg-white relative overflow-hidden w-full border border-gray-200 rounded px-5 py-3 mb-5">
+      <ul
+        style={{
+          width: 80 + usersContainerWidth,
+          transform: `translateX(${translateX}px)`
+        }}
+        className="flex space-x-3 transition-all duration-700 ease-in-out"
+      >
+        {USER_STORIES.map(user => (
+          <li
+            key={user._id}
+            className="w-16 overflow-hidden truncate space-y-5 text-center"
+          >
+            <Link to={`/stories/${user._id}`}>
+              <div className="bg-gradient-to-tr from-yellow-400 to-fuchsia-600 p-0.5 rounded-full">
+                <div className="bg-white p-0.5 rounded-full">
+                  <img
+                    className="w-full h-full object-cover rounded-full"
+                    src={`/img/users/${user.username}.jpg`}
+                    alt={user.username}
+                  />
+                </div>
               </div>
-            </div>
-            <span className="text-xs">russcodes</span>
-          </Link>
-        </li>
-        <li className="w-16 overflow-hidden space-y-5 text-center">
-          <Link to="/">
-            <div className="bg-gradient-to-tr from-yellow-400 to-fuchsia-600 p-0.5 rounded-full">
-              <div className="bg-white p-0.5 rounded-full">
-                <img
-                  className="w-full h-full object-cover rounded-full"
-                  src="/img/users/onlysiamak.jpg"
-                  alt="onlysiamak"
-                />
-              </div>
-            </div>
-            <span className="text-xs">onlysiamak</span>
-          </Link>
-        </li>
-        <li className="w-16 overflow-hidden space-y-5 text-center">
-          <Link to="/">
-            <div className="bg-gradient-to-tr from-yellow-400 to-fuchsia-600 p-0.5 rounded-full">
-              <div className="bg-white p-0.5 rounded-full">
-                <img
-                  className="w-full h-full object-cover rounded-full"
-                  src="/img/users/sepide_moqadasi.jpg"
-                  alt="sepide_moqadasi"
-                />
-              </div>
-            </div>
-            <span className="text-xs">sepide_moqadasi</span>
-          </Link>
-        </li>
-        <li className="w-16 overflow-hidden space-y-5 text-center">
-          <Link to="/">
-            <div className="bg-gradient-to-tr from-yellow-400 to-fuchsia-600 p-0.5 rounded-full">
-              <div className="bg-white p-0.5 rounded-full">
-                <img
-                  className="w-full h-full object-cover rounded-full"
-                  src="/img/users/ui.amjad.jpg"
-                  alt="ui.amjad"
-                />
-              </div>
-            </div>
-            <span className="text-xs">ui.amjad</span>
-          </Link>
-        </li>
-        <li className="w-16 overflow-hidden space-y-5 text-center">
-          <Link to="/">
-            <div className="bg-gradient-to-tr from-yellow-400 to-fuchsia-600 p-0.5 rounded-full">
-              <div className="bg-white p-0.5 rounded-full">
-                <img
-                  className="w-full h-full object-cover rounded-full"
-                  src="/img/users/mehradhiddenofficial.jpg"
-                  alt="mehradhiddenofficial"
-                />
-              </div>
-            </div>
-            <span className="text-xs">mehradhiddenofficial</span>
-          </Link>
-        </li>
-        <li className="w-16 overflow-hidden space-y-5 text-center">
-          <Link to="/">
-            <div className="bg-gradient-to-tr from-yellow-400 to-fuchsia-600 p-0.5 rounded-full">
-              <div className="bg-white p-0.5 rounded-full">
-                <img
-                  className="w-full h-full object-cover rounded-full"
-                  src="/img/users/fazurrehman.jpg"
-                  alt="fazurrehman"
-                />
-              </div>
-            </div>
-            <span className="text-xs">fazurrehman</span>
-          </Link>
-        </li>
-        <li className="w-16 overflow-hidden space-y-5 text-center">
-          <Link to="/">
-            <div className="bg-gradient-to-tr from-yellow-400 to-fuchsia-600 p-0.5 rounded-full">
-              <div className="bg-white p-0.5 rounded-full">
-                <img
-                  className="w-full h-full object-cover rounded-full"
-                  src="/img/users/sajon.co.jpg"
-                  alt="sajon.co"
-                />
-              </div>
-            </div>
-            <span className="text-xs">sajon.co</span>
-          </Link>
-        </li>
+              <span className="text-xs">{user.username}</span>
+            </Link>
+          </li>
+        ))}
       </ul>
+
+      <button
+        onClick={gotoPrev}
+        disabled={translateX === 0}
+        style={{
+          opacity: translateX < 0 ? 1 : 0,
+          boxShadow: "0 0 10px 2px rgba(0,0,0,0.075)"
+        }}
+        className="absolute left-5 top-1/2 transform -translate-y-1/2 w-6 h-6 bg-white rounded-full grid place-items-center transition duration-150 ease-in-out hover:scale-110"
+      >
+        <HiChevronLeft />
+      </button>
+
+      <button
+        onClick={gotoNext}
+        style={{ boxShadow: "0 0 10px 2px rgba(0,0,0,0.075)" }}
+        className="absolute right-5 top-1/2 transform -translate-y-1/2 w-6 h-6 bg-white rounded-full grid place-items-center transition duration-150 ease-in-out hover:scale-110"
+      >
+        <HiChevronRight />
+      </button>
     </section>
   )
 }
-
-export default Story
