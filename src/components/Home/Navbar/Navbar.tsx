b883a7c3a0f353851abddaaa6fad24ec877f1c84
Mehdi Neysi <dev.mehdineysi@gmail.com>
2022-04-08 21:58:41 +0430

    fix: add z-index to navbar

--- src/components/Home/Navbar/Navbar.tsx
+++ src/components/Home/Navbar/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
 
   return (
     <>
-      <header className="bg-white py-2 border-b border-gray-200 fixed top-0 left-0 w-full">
+      <header className="bg-white py-2 border-b border-gray-200 fixed top-0 left-0 w-full z-10">
         <div className="main-header__wrapper">
           <div className="hidden md:flex max-w-4xl mx-auto items-center px-5 lg:px-0">
             <Logo />
